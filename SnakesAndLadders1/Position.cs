72d671db1514979b361e22a130eeab641747ddab
Byves94 <92880306+Byves94@users.noreply.github.com>
2022-06-18 11:07:28 +0300

    Update Position.cs

--- SnakesAndLadders1/Position.cs
+++ SnakesAndLadders1/Position.cs
@@ -10,7 +10,6 @@ namespace SnakesAndLadders1
     {
         public bool IsSnake { get; set; } = false;
         public bool IsLadder { get; set; } = false;
-        public bool ContainPlayer { get; set; } = false;
         public bool IsGold { get; set; } = false;
         public int PositionNumber { get; set; }
         public int newPositionNumber { get; set; }
